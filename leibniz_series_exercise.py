446ae2253425471c1d8c2199f12971c0f3798b87
212543631 <adi.shefer.43@gmail.com>
2025-11-13 16:30:20 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,13 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    list_of_numbers = []
+    sum = 0
+    for i in range(n_terms):
+        list_of_numbers.append(i)
+        list_of_numbers[i] = ((-1)**i)/(2*i+1)
+        sum += list_of_numbers[i]
+    return sum
+
+
+        
+
+
